7280ad79e5a9051a5e6a45a91be7a7df7e384f41
Steven Abaco <stevenabaco@gmail.com>
2021-05-17 22:50:59 -0500

    Add logic to start server

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const data = require('./data.json');
+const port = 3000
 //Setup middleware
 app.set('view engine', 'pug');
 app.use('/static', express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log(`The app is running on port ${port}`);
+})
