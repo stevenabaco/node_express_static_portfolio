3d59a756ac2a92c06806a82268e55138d5de40db
Steven Abaco <stevenabaco@gmail.com>
2021-05-19 17:47:08 -0500

    Add Error Handling and update styles

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const data = require('./data.json');
-const port = 3000
+const port = process.env.port || 3000;
 
-// Middleware configutration
+// Confifure view engine to PUG
 app.set('view engine', 'pug');
+
+// Configure static folders
 app.use('/static', express.static(path.join(__dirname, 'public')));
+
+// Configure URL encoding with express
+app.use(express.urlencoded({ extended: false }));
 
 // Routing configuration
 const mainRoutes = require('./routes');
@@ -16,6 +21,25 @@ const projectRoutes = require('./routes/projects');
 app.use(mainRoutes);
 app.use('/project', projectRoutes);
 
+// Error handling
+
+app.use((req, res, next) => {
+	const err = new Error('Your request was not found!');
+	err.status = 404;
+	next(err);
+});
+
+app.use((err, req, res, next) => {
+	if (err.status === 404) {
+		console.log(err.message);
+		res.status(404).render('404', { err });
+	} else {
+		err.message = 'Sorry! Looks like something went wrong.';
+		res.render('error', { err });
+	}
+});
+
+// Start Server
 app.listen(3000, () => {
-  console.log(`The app is running on port ${port}`);
-})
+	console.log(`The app is running on port ${port}`);
+});
--- routes/projects.js
+++ routes/projects.js
@@ -5,26 +5,28 @@ const data = require('../data.json');
 // Extract "projects" objects from data.json
 const { projects } = data;
 
-
 // Projects route based on the "id" of the project
 router.get('/:id', (req, res, next) => {
 	// Pull "id" from the request parameters
 	const { id } = req.params;
 	// Pull data from project with matching id
 	const projectData = projects[id];
-  if (id) {
-    res.render('project', {
-		project: projectData.project_name,
-		description: projectData.description,
-		technologies: projectData.technologies,
-		images: projectData.image_urls,
-		demo: projectData.live_link,
-		github: projectData.github_link,
-	});
-  } else {
-    next()
-  }
-	
+	if (projectData) {
+		res.render('project', {
+			project: projectData.project_name,
+			description: projectData.description,
+			technologies: projectData.technologies,
+			images: projectData.image_urls,
+			demo: projectData.live_link,
+			github: projectData.github_link,
+		});
+	} else {
+		// Create error in the event of page not found, console log and pass it through with "next()"
+		const err = new Error('The project you selected does not exist');
+		console.log('project not found');
+		err.status = 404;
+		next(err);
+	}
 });
 
 module.exports = router;
